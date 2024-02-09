096e6e4758bd04f483b12e2d017442d0ba733cf4
Nat <laura.rojas-r@mail.escuelaing.edu.co>
2024-02-09 14:34:45 -0500

    trabajo hecho en clase

---
+++ src/main/java/edu/eci/cvds/patterns/archetype/App.java
@@ -0,0 +1,19 @@
+package edu.eci.cvds.patterns.archetype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello world!");
+        }
+    }
+
+}
